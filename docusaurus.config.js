e8b33144c023dbb4f8b145434762be535151bc3d
tempname0606 <6697@protonmail.ch>
2023-08-27 16:59:25 +0100

    changed stuff

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Notes',
-  tagline: 'To be changed...',
+  tagline: 'To be changed.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
